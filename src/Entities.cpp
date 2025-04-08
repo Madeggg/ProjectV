ad010f0e071fc296949b7ed9f0f8bee7b8a3122f
FrancoisAllie <152420852+FrancoisAllie@users.noreply.github.com>
2025-04-08 20:21:13 +0200

    Ajout de la classe Weapon

--- src/Entities.cpp
+++ src/Entities.cpp
@@ -4,9 +4,10 @@
 #include <QGraphicsScene>
 #include <QPixmap>
 #include <QRandomGenerator>
+#include <QStringList>
 
 Enemy::Enemy(EnemyType type, QGraphicsItem* parent) : QGraphicsPixmapItem(parent), type(type){
-    setAppearance();
+    setAppearance();            // On définit le sprite + comportement visuel dès la création 
 }
 
 void Enemy::setAppearance(){
@@ -14,15 +15,16 @@ void Enemy::setAppearance(){
 
     if (type == EnemyType::Soldier) {
         imagePath = " ";                                    // Sprite unique pour les soldats
-    } else if (type == EnemyType::SuperHero) {
-        QStringList heroSprites = {
+    } else if (type == EnemyType::Hero) {
+        QStringList heroSprites = {                         
             "homelander",
             "a_train",
-            "black_noir",                   //Exemple d'une liste contenant les sprites de super héros
+            "black_noir",                   //Exemple d'une liste QStringList contenant les sprites de super héros (des strings vers les liens des sprites)
             "starlight",
             "the_deep"
         };
 
+        // Choix aléatoire d’un héros dans la liste de sprite 
         int index = QRandomGenerator::global()->bounded(heroSprites.size());
         QString chosenHero = heroSprites[index];
         imagePath = ":/img/super_" + chosenHero + ".png";
@@ -30,11 +32,27 @@ void Enemy::setAppearance(){
 
     }
 
-    QPixmap sprite(imagePath);
+    QPixmap sprite(imagePath);  
     if (!sprite.isNull()) {
-        setPixmap(sprite.scaled(64, 64)); // pixelart, taille à adapter
+        setPixmap(sprite.scaled(64, 64)); // La fonction scale permet d'appliquer au pixmap la taille donnée 
     } else {
         qDebug("Image introuvable : %s", qPrintable(imagePath));
     }
 }
+
+
+Weapon::Weapon(QString name, int damage, QString projectileSprite): 
+    name(name), damage(damage), projectileSpritePath(projectileSprite) {}
+
+//Accesseurs
+QString Weapon::getName() const { return name; }
+int Weapon::getDamage() const { return damage; }
+QString Weapon::getProjectileSprite() const { return projectileSpritePath; }
+
+void Entity::setWeapon(Weapon* w) {
+    weapon = w;
+}
+
+Weapon* Entity::getWeapon() const {
+    return weapon;
 }
--- src/Entities.h
+++ src/Entities.h
@@ -6,11 +6,50 @@
 #include <QGraphicsPixmapItem>
 #include <QObject>
 #include <QTimer>
+#include <QString>
 
-class Enemy: public QGraphicsPixmapItem{
+enum class EnemyType{           //On crée une classe à part pour les deux types d'ennemis 
+    Soldier,
+    Hero
+};
+
+class Enemy: public QGraphicsPixmapItem{        
     Q_OBJECT
 
-    public:
-        
-}
+public:
+    // Constructeur : crée un ennemi du type spécifié
+    Enemy(EnemyType type,QGraphicsItem* parent = nullptr);
+    //Mutateur
+    void setWeapon(Weapon* weapon);
+    //Accesseur
+    Weapon* getWeapon() const;
 
+public slot : 
+    void move();            //Méthode qui permettra à l'ennemi de se déplacer de manière aléatoire
+
+private :
+    EnemyType* type;
+    QTimer* moveTimer;
+    
+    void setAppearance();       //Cette méthode permettra de donner l'apparence à l'ennemi en fonction de son type 
+
+
+};
+
+
+class Weapon{
+    public:
+        Weapon(QString name, int damage, QString projectileSprite); //Constucteur de l'arme
+
+        //Accesseurs
+        QString getName() const;
+        int getDamage() const;
+        QString getProjectileSprite() const;
+    
+    private:
+        QString name;
+        int damage;
+        QString projectileSpritePath; // pour les visuels de tirs
+};
+
+#endif 
