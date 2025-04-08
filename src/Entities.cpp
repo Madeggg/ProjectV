4a5737fecf8671cb0a59975fa13c80a41065e4cc
FrancoisAllie <152420852+FrancoisAllie@users.noreply.github.com>
2025-04-08 16:35:51 +0200

    Entities.h + Entities.cpp : premières idées

--- src/Entities.cpp
+++ src/Entities.cpp
@@ -1,1 +1,40 @@
 // Cette page sert à tout ce qui est hostile (enemies, bombes etc..) + les projectiles tirés par joueur ou ennemis + les armes
+
+#include "Entities.h"
+#include <QGraphicsScene>
+#include <QPixmap>
+#include <QRandomGenerator>
+
+Enemy::Enemy(EnemyType type, QGraphicsItem* parent) : QGraphicsPixmapItem(parent), type(type){
+    setAppearance();
+}
+
+void Enemy::setAppearance(){
+    QString imagePath;              //Déclaration du lien vers l'image comme un objet QString
+
+    if (type == EnemyType::Soldier) {
+        imagePath = " ";                                    // Sprite unique pour les soldats
+    } else if (type == EnemyType::SuperHero) {
+        QStringList heroSprites = {
+            "homelander",
+            "a_train",
+            "black_noir",                   //Exemple d'une liste contenant les sprites de super héros
+            "starlight",
+            "the_deep"
+        };
+
+        int index = QRandomGenerator::global()->bounded(heroSprites.size());
+        QString chosenHero = heroSprites[index];
+        imagePath = ":/img/super_" + chosenHero + ".png";
+
+
+    }
+
+    QPixmap sprite(imagePath);
+    if (!sprite.isNull()) {
+        setPixmap(sprite.scaled(64, 64)); // pixelart, taille à adapter
+    } else {
+        qDebug("Image introuvable : %s", qPrintable(imagePath));
+    }
+}
+}
--- src/Entities.h
+++ src/Entities.h
@@ -1,1 +1,16 @@
 // Cette page sert à tout ce qui est hostile (enemies, bombes etc..) + les projectiles tirés par joueur ou ennemis + les armes
+
+#ifndef ENTITIES_H
+#define ENTITIES_H
+
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QTimer>
+
+class Enemy: public QGraphicsPixmapItem{
+    Q_OBJECT
+
+    public:
+        
+}
+
