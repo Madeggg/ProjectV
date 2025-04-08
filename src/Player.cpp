e219a6be7ad1160f856757ba99a722a8e66f0b2c
Madeggg <152902274+Madeggg@users.noreply.github.com>
2025-04-08 15:21:39 +0200

    Implémentation de Player.cpp et Player.h

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,1 +1,24 @@
 // Cette page est la afin de gérer le Personnage principal, et les controles par le joueur
+
+#include "Player.h"
+#include <QPixmap>
+
+Player::Player(QGraphicsItem* parent) : QObject(), QGraphicsPixmapItem(parent), health(100), speed(5) { //appel des constructeur de QObject et QGraphicsPixmapItem
+    setPixmap(QPixmap(":/images/player.png").scaled(40, 40));
+}
+// Méthodes de déplacement 
+void Player::moveLeft() {
+    setPos(x() - speed, y());
+}
+
+void Player::moveRight() {
+    setPos(x() + speed, y());
+}
+
+void Player::moveUp() {
+    setPos(x(), y() - speed);
+}
+
+void Player::moveDown() {
+    setPos(x(), y() + speed);
+}
--- src/Player.h
+++ src/Player.h
@@ -1,1 +1,31 @@
 // Cette page est la afin de gérer le Personnage principal, et les controles par le joueur
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <QGraphicsPixmapItem>
+#include <QObject>
+#include <QKeyEvent>
+#include <QTimer>
+
+class Player : public QObject, public QGraphicsPixmapItem { //QGraphicsPixmapItem, sous-classe de QGraphicsItem, affiche une image (pixmap) dans une scène graphique
+    Q_OBJECT
+
+public:
+    Player(QGraphicsItem* parent = nullptr); //constructeur de la classe, cet objet devient le parent de notre Player
+
+    //Méthodes de déplacement 
+    void moveLeft();
+    void moveRight();
+    void moveUp();
+    void moveDown();
+    // Méthodes de gestion de PV
+    int getHealth() const;
+    void setHealth(int newHealth);
+    void takeDamage(int amount);
+
+private:
+    int health;
+    int speed;
+};
+
+#endif 
